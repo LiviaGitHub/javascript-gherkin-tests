a7ddae3addd956bce0a2660000ebdebf23b3cd57
LiviaGitHub <liviagithub@gmail.com>
2022-09-20 16:41:45 +0200

    first_commit

--- cypress/e2e/factorial_calculator.cy.js
+++ cypress/e2e/factorial_calculator.cy.js
@@ -4,4 +4,22 @@ describe('factorial calculator tests', () => {
     cy.clickGetFactorialButton();
     cy.result('The factorial of 99 is: 9.332621544394415e+155');
   })
+
+  it('check string as value', () => {
+    cy.fieldEnterAInteger('test');
+    cy.clickGetFactorialButton();
+    cy.result('Please enter an integer');
+  })
+
+  it('check special characters as value', () => {
+    cy.fieldEnterAInteger('@#@$%%');
+    cy.clickGetFactorialButton();
+    cy.result('Please enter an integer');
+  })
+
+  it('calculate values between 1 and 9', () => {
+    cy.fieldEnterAInteger(1);
+    cy.clickGetFactorialButton();
+    cy.result('The factorial of 1 is: 1');
+  })
 });
